67eb022a4f839115f56db7e19ce99a1bf32d61b6
Showkat Easin <68397684+ShowkatEasin@users.noreply.github.com>
2022-09-30 00:20:25 +0600

    Complete

--- script.js
+++ script.js
@@ -2,18 +2,555 @@ function result(){
          var tMark=document.getElementById("tMark").value;
          var pMark=document.getElementById("pMark").value;
          var total = (parseInt(tMark) + parseInt(pMark));
-        
+         var grade ="";
+         var point ="";
     document.getElementById("totalMark").value=total;
+    
     if (total >=80 && total <=100){
-        var grade ="A+";
-        var point ="5.00";
+         grade ="A+";
+         point ="5.00";
     }
     else if(total >=70 && total <=79){
-        var grade ="A";
-        var point ="4.50";
+         grade ="A";
+         point ="4.00";
     }
 
-        document.getElementById("grade").value="A+";
-        document.getElementById("point").value="5.00";
+    else if(total >=60 && total <=69){
+         grade ="A-";
+         point ="3.50";
+    }
+    else if(total >=50 && total <=59){
+         grade ="B";
+         point ="2.50";
+    }
+    else if(total >=33 && total <=49){
+         grade ="C";
+         point ="2.00";
+    }
+    else if(total >=0 && total <=33){
+         grade ="F";
+         point ="0.00";
+    }
+    else {
+        alert ("Wrong Mark");
+        total="";
+         grade ="";
+         point ="";
+         point = "";
+        document.getElementById("tMark").value="";
+        document.getElementById("pMark").value="";
 
+    }
+
+        document.getElementById("totalMark").value=total;
+        document.getElementById("grade").value=grade;
+        document.getElementById("point").value=point;
+}
+
+// Bangla-II
+
+function result1(){
+    var tMark=document.getElementById("tMark1").value;
+    var pMark=document.getElementById("pMark1").value;
+    var total = (parseInt(tMark) + parseInt(pMark));
+    var grade ="";
+    var point ="";
+document.getElementById("totalMark1").value=total;
+
+if (total >=80 && total <=100){
+    grade ="A+";
+    point ="5.00";
+}
+else if(total >=70 && total <=79){
+    grade ="A";
+    point ="4.00";
+}
+
+else if(total >=60 && total <=69){
+    grade ="A-";
+    point ="3.50";
+}
+else if(total >=50 && total <=59){
+    grade ="B";
+    point ="2.50";
+}
+else if(total >=33 && total <=49){
+    grade ="C";
+    point ="2.00";
+}
+else if(total >=0 && total <=33){
+    grade ="F";
+    point ="0.00";
+}
+else {
+   alert ("Wrong Mark");
+   total="";
+    grade ="";
+    point ="";
+    point = "";
+   document.getElementById("tMark1").value="";
+   document.getElementById("pMark1").value="";
+
+}
+
+   document.getElementById("totalMark1").value=total;
+   document.getElementById("grade1").value=grade;
+   document.getElementById("point1").value=point;
+}
+// English-I
+
+function result2(){
+    var tMark=document.getElementById("tMark2").value;
+    var pMark=document.getElementById("pMark2").value;
+    var total = (parseInt(tMark) + parseInt(pMark));
+    var grade ="";
+    var point ="";
+document.getElementById("totalMark2").value=total;
+
+if (total >=80 && total <=100){
+    grade ="A+";
+    point ="5.00";
+}
+else if(total >=70 && total <=79){
+    grade ="A";
+    point ="4.00";
+}
+
+else if(total >=60 && total <=69){
+    grade ="A-";
+    point ="3.50";
+}
+else if(total >=50 && total <=59){
+    grade ="B";
+    point ="2.50";
+}
+else if(total >=33 && total <=49){
+    grade ="C";
+    point ="2.00";
+}
+else if(total >=0 && total <=33){
+    grade ="F";
+    point ="0.00";
+}
+else {
+   alert ("Wrong Mark");
+   total="";
+    grade ="";
+    point ="";
+    point = "";
+   document.getElementById("tMark2").value="";
+   document.getElementById("pMark2").value="";
+
+}
+
+   document.getElementById("totalMark2").value=total;
+   document.getElementById("grade2").value=grade;
+   document.getElementById("point2").value=point;
+}
+//  English-II
+
+function result3(){
+    var tMark=document.getElementById("tMark3").value;
+    var pMark=document.getElementById("pMark3").value;
+    var total = (parseInt(tMark) + parseInt(pMark));
+    var grade ="";
+    var point ="";
+document.getElementById("totalMark3").value=total;
+
+if (total >=80 && total <=100){
+    grade ="A+";
+    point ="5.00";
+}
+else if(total >=70 && total <=79){
+    grade ="A";
+    point ="4.00";
+}
+
+else if(total >=60 && total <=69){
+    grade ="A-";
+    point ="3.50";
+}
+else if(total >=50 && total <=59){
+    grade ="B";
+    point ="2.50";
+}
+else if(total >=33 && total <=49){
+    grade ="C";
+    point ="2.00";
+}
+else if(total >=0 && total <=33){
+    grade ="F";
+    point ="0.00";
+}
+else {
+   alert ("Wrong Mark");
+   total="";
+    grade ="";
+    point ="";
+    point = "";
+   document.getElementById("tMark3").value="";
+   document.getElementById("pMark3").value="";
+
+}
+
+   document.getElementById("totalMark3").value=total;
+   document.getElementById("grade3").value=grade;
+   document.getElementById("point3").value=point;
+
+}
+// Physics -I
+
+function result4(){
+    var tMark=document.getElementById("tMark4").value;
+    var pMark=document.getElementById("pMark4").value;
+    var total = (parseInt(tMark) + parseInt(pMark));
+    var grade ="";
+    var point ="";
+document.getElementById("totalMark4").value=total;
+
+if (total >=80 && total <=100){
+    grade ="A+";
+    point ="5.00";
+}
+else if(total >=70 && total <=79){
+    grade ="A";
+    point ="4.00";
+}
+
+else if(total >=60 && total <=69){
+    grade ="A-";
+    point ="3.50";
+}
+else if(total >=50 && total <=59){
+    grade ="B";
+    point ="2.50";
+}
+else if(total >=33 && total <=49){
+    grade ="C";
+    point ="2.00";
+}
+else if(total >=0 && total <=33){
+    grade ="F";
+    point ="0.00";
+}
+else {
+   alert ("Wrong Mark");
+   total="";
+    grade ="";
+    point ="";
+    point = "";
+   document.getElementById("tMark4").value="";
+   document.getElementById("pMark4").value="";
+
+}
+
+   document.getElementById("totalMark4").value=total;
+   document.getElementById("grade4").value=grade;
+   document.getElementById("point4").value=point;
+
+}
+
+// Physics-II
+
+function result5(){
+    var tMark=document.getElementById("tMark5").value;
+    var pMark=document.getElementById("pMark5").value;
+    var total = (parseInt(tMark) + parseInt(pMark));
+    var grade ="";
+    var point ="";
+document.getElementById("totalMark5").value=total;
+
+if (total >=80 && total <=100){
+    grade ="A+";
+    point ="5.00";
+}
+else if(total >=70 && total <=79){
+    grade ="A";
+    point ="4.00";
+}
+
+else if(total >=60 && total <=69){
+    grade ="A-";
+    point ="3.50";
+}
+else if(total >=50 && total <=59){
+    grade ="B";
+    point ="2.50";
+}
+else if(total >=33 && total <=49){
+    grade ="C";
+    point ="2.00";
+}
+else if(total >=0 && total <=33){
+    grade ="F";
+    point ="0.00";
+}
+else {
+   alert ("Wrong Mark");
+   total="";
+    grade ="";
+    point ="";
+    point = "";
+   document.getElementById("tMark5").value="";
+   document.getElementById("pMark5").value="";
+
+}
+
+   document.getElementById("totalMark5").value=total;
+   document.getElementById("grade5").value=grade;
+   document.getElementById("point5").value=point;
+
+}
+// Chemistry-I
+
+function result6(){
+    var tMark=document.getElementById("tMark6").value;
+    var pMark=document.getElementById("pMark6").value;
+    var total = (parseInt(tMark) + parseInt(pMark));
+    var grade ="";
+    var point ="";
+document.getElementById("totalMark6").value=total;
+
+if (total >=80 && total <=100){
+    grade ="A+";
+    point ="5.00";
+}
+else if(total >=70 && total <=79){
+    grade ="A";
+    point ="4.00";
+}
+
+else if(total >=60 && total <=69){
+    grade ="A-";
+    point ="3.50";
+}
+else if(total >=50 && total <=59){
+    grade ="B";
+    point ="2.50";
+}
+else if(total >=33 && total <=49){
+    grade ="C";
+    point ="2.00";
+}
+else if(total >=0 && total <=33){
+    grade ="F";
+    point ="0.00";
+}
+else {
+   alert ("Wrong Mark");
+   total="";
+    grade ="";
+    point ="";
+    point = "";
+   document.getElementById("tMark6").value="";
+   document.getElementById("pMark6").value="";
+
+}
+
+   document.getElementById("totalMark6").value=total;
+   document.getElementById("grade6").value=grade;
+   document.getElementById("point6").value=point;
+}
+
+// <!-- Chemisrty-II -->
+
+function result7(){
+    var tMark=document.getElementById("tMark7").value;
+    var pMark=document.getElementById("pMark7").value;
+    var total = (parseInt(tMark) + parseInt(pMark));
+    var grade ="";
+    var point ="";
+document.getElementById("totalMark7").value=total;
+
+if (total >=80 && total <=100){
+    grade ="A+";
+    point ="5.00";
+}
+else if(total >=70 && total <=79){
+    grade ="A";
+    point ="4.00";
+}
+
+else if(total >=60 && total <=69){
+    grade ="A-";
+    point ="3.50";
+}
+else if(total >=50 && total <=59){
+    grade ="B";
+    point ="2.50";
+}
+else if(total >=33 && total <=49){
+    grade ="C";
+    point ="2.00";
+}
+else if(total >=0 && total <=33){
+    grade ="F";
+    point ="0.00";
+}
+else {
+   alert ("Wrong Mark");
+   total="";
+    grade ="";
+    point ="";
+    point = "";
+   document.getElementById("tMark7").value="";
+   document.getElementById("pMark7").value="";
+
+}
+
+   document.getElementById("totalMark7").value=total;
+   document.getElementById("grade7").value=grade;
+   document.getElementById("point7").value=point;
+}
+
+// Bio-I
+
+function result8(){
+    var tMark=document.getElementById("tMark8").value;
+    var pMark=document.getElementById("pMark8").value;
+    var total = (parseInt(tMark) + parseInt(pMark));
+    var grade ="";
+    var point ="";
+document.getElementById("totalMark8").value=total;
+
+if (total >=80 && total <=100){
+    grade ="A+";
+    point ="5.00";
+}
+else if(total >=70 && total <=79){
+    grade ="A";
+    point ="4.00";
+}
+
+else if(total >=60 && total <=69){
+    grade ="A-";
+    point ="3.50";
+}
+else if(total >=50 && total <=59){
+    grade ="B";
+    point ="2.50";
+}
+else if(total >=33 && total <=49){
+    grade ="C";
+    point ="2.00";
+}
+else if(total >=0 && total <=33){
+    grade ="F";
+    point ="0.00";
+}
+else {
+   alert ("Wrong Mark");
+   total="";
+    grade ="";
+    point ="";
+    point = "";
+   document.getElementById("tMark8").value="";
+   document.getElementById("pMark8").value="";
+
+}
+
+   document.getElementById("totalMark8").value=total;
+   document.getElementById("grade8").value=grade;
+   document.getElementById("point8").value=point;
+}
+
+// Bio-II
+
+function result9(){
+    var tMark=document.getElementById("tMark9").value;
+    var pMark=document.getElementById("pMark9").value;
+    var total = (parseInt(tMark) + parseInt(pMark));
+    var grade ="";
+    var point ="";
+document.getElementById("totalMark9").value=total;
+
+if (total >=80 && total <=100){
+    grade ="A+";
+    point ="5.00";
+}
+else if(total >=70 && total <=79){
+    grade ="A";
+    point ="4.00";
+}
+
+else if(total >=60 && total <=69){
+    grade ="A-";
+    point ="3.50";
+}
+else if(total >=50 && total <=59){
+    grade ="B";
+    point ="2.50";
+}
+else if(total >=33 && total <=49){
+    grade ="C";
+    point ="2.00";
+}
+else if(total >=0 && total <=33){
+    grade ="F";
+    point ="0.00";
+}
+else {
+   alert ("Wrong Mark");
+   total="";
+    grade ="";
+    point ="";
+    point = "";
+   document.getElementById("tMark9").value="";
+   document.getElementById("pMark").value="";
+
+}
+
+   document.getElementById("totalMark9").value=total;
+   document.getElementById("grade9").value=grade;
+   document.getElementById("point9").value=point;
+}
+// ICT
+
+function result10(){
+    var tMark=document.getElementById("tMark10").value;
+    var pMark=document.getElementById("pMark10").value;
+    var total = (parseInt(tMark) + parseInt(pMark));
+    var grade ="";
+    var point ="";
+document.getElementById("totalMark10").value=total;
+
+if (total >=80 && total <=100){
+    grade ="A+";
+    point ="5.00";
+}
+else if(total >=70 && total <=79){
+    grade ="A";
+    point ="4.00";
+}
+
+else if(total >=60 && total <=69){
+    grade ="A-";
+    point ="3.50";
+}
+else if(total >=50 && total <=59){
+    grade ="B";
+    point ="2.50";
+}
+else if(total >=33 && total <=49){
+    grade ="C";
+    point ="2.00";
+}
+else if(total >=0 && total <=33){
+    grade ="F";
+    point ="0.00";
+}
+else {
+   alert ("Wrong Mark");
+   total="";
+    grade ="";
+    point ="";
+    point = "";
+   document.getElementById("tMark10").value="";
+   document.getElementById("pMark10").value="";
+
+}
+
+   document.getElementById("totalMark10").value=total;
+   document.getElementById("grade10").value=grade;
+   document.getElementById("point10").value=point;
 }
