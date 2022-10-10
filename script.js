2cf81d2ef1cfe274d1d0c323ebd01098ecce62a2
Showkat Easin <68397684+ShowkatEasin@users.noreply.github.com>
2022-10-10 20:16:24 +0600

    phy1

--- script.js
+++ script.js
@@ -703,14 +703,14 @@ function Mark(totalMark) {
 
 }
 
-function english2() {
-    var _tMark3 = document.getElementById("tMark3").value;
-    var _pMark3 = document.getElementById("pMark3").value;
-    var _totalMark3 = (parseInt(_tMark3) + parseInt(_pMark3));
-    Mark(_totalMark3);
-    document.getElementById("totalMark3").value=_totalMark3;
-    document.getElementById("grade3").value = grade;
-    document.getElementById("point3").value = point;
+function english1() {
+    var _tMark2 = document.getElementById("tMark2").value;
+    var _pMark2 = document.getElementById("pMark2").value;
+    var _totalMark2 = (parseInt(_tMark2) + parseInt(_pMark2));
+    Mark(_totalMark2);
+    document.getElementById("totalMark2").value=_totalMark2;
+    document.getElementById("grade2").value = grade;
+    document.getElementById("point2").value = point;
 }
 
 
@@ -754,14 +754,65 @@ function Mark(totalMark) {
 
 }
 
-function english1() {
-    var _tMark2 = document.getElementById("tMark2").value;
-    var _pMark2 = document.getElementById("pMark2").value;
-    var _totalMark2 = (parseInt(_tMark2) + parseInt(_pMark2));
-    Mark(_totalMark2);
-    document.getElementById("totalMark2").value=_totalMark2;
-    document.getElementById("grade2").value = grade;
-    document.getElementById("point2").value = point;
+function english2() {
+    var _tMark3 = document.getElementById("tMark3").value;
+    var _pMark3 = document.getElementById("pMark3").value;
+    var _totalMark3 = (parseInt(_tMark3) + parseInt(_pMark3));
+    Mark(_totalMark3);
+    document.getElementById("totalMark3").value=_totalMark3;
+    document.getElementById("grade3").value = grade;
+    document.getElementById("point3").value = point;
+}
+
+// physics-1
+
+
+var grade="";
+var point="";
+
+function Mark(totalMark) {
+    if(totalMark >=80 & totalMark <=100){
+        grade="A+";
+        point="5.00";
+    }
+    else if(totalMark >=70 & totalMark <=79){
+        grade="A";
+        point="4.00";
+    }
+    else if(totalMark >=60 & totalMark <=69){
+        grade="A-";
+        point="3.50";
+    }
+    else if(totalMark >=50 & totalMark <=59){
+        grade="B";
+        point="3.00";
+    }
+    else if(totalMark >=40 & totalMark <=49){
+        grade="C";
+        point="2.00";
+    }
+    else if(totalMark >=33 & totalMark <=39){
+        grade="D";
+        point="1.00";
+    }
+    else if(totalMark >=0 & totalMark <=32){
+        grade="F";
+        point="0.00";
+    }
+    else{
+        alert("Wrong Type");
+    }
+
+}
+
+function physics1() {
+    var _tMark4 = document.getElementById("tMark4").value;
+    var _pMark4 = document.getElementById("pMark4").value;
+    var _totalMark4 = (parseInt(_tMark4) + parseInt(_pMark4));
+    Mark(_totalMark4);
+    document.getElementById("totalMark4").value=_totalMark4;
+    document.getElementById("grade4").value = grade;
+    document.getElementById("point4").value = point;
 }
 
 
